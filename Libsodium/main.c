95c5bd8f25f42e4282342d5a09f7f20ebefcbaaf
SergioR <sergio.rmz9811@gmail.com>
2024-09-01 19:40:24 -0600

    Se agrega ejemplo de uso de Libreria Sodium

---
+++ Libsodium/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "sodium.h"
+#include "encrypt.h"
+
+int main(void)
+{
+  int i = 0;
+  unsigned char buf[128];
+  if (sodium_init() == -1) {
+      return 1;
+  }
+ 
+  randombytes_buf(buf, sizeof((buf)));
+  printf("%lu\n",sizeof(buf));
+  for (i = 0; i < sizeof(buf); i++)
+	printf("%02X",(unsigned int)buf[i]);
+  printf("\n");
+
+}
